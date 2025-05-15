11b03d83513f8b099c1b17efc5df2ebabd4dccec
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-15 11:05:36 +0200

    ensure requests return content-type as xml if forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -9,4 +9,12 @@ describe('Content-Type Middleware', () => {
     const res = await request(app).get('/test_content_type').expect('content-type', /json/)
     expect(res.headers['content-type']).toBe('application/json; charset=utf-8')
   })
+  test('Should return xml content-type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.set('content-type', 'text/xml')
+      res.send('')
+    })
+    const res = await request(app).get('/test_content_type_xml').expect('content-type', /xml/)
+    expect(res.headers['content-type']).toBe('text/xml; charset=utf-8')
+  })
 })
