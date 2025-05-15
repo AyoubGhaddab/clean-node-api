3fac0db43bc06204bb8fdd1f8717d4e3d72b5984
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-15 11:43:49 +0200

    connect to mongodb before listening for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,11 @@
-const app = require('./config/app')
-
-app.listen(5858, () => {
-  console.log('Server is running on port 5858')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
+MongoHelper.connect(env.mongoUrl).then(() => {
+  const app = require('./config/app')
+  app.listen(5858, () => {
+    console.log('Server is running on port 5858')
+  })
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error)
+  process.exit(1)
 })
