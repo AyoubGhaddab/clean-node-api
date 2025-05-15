f1e51ad17dbc20ae9d3309871ed44153b3325540
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-15 13:46:30 +0200

    fix mongodb port and app port

--- src/main/index.js
+++ src/main/index.js
@@ -2,8 +2,8 @@ const MongoHelper = require('../infra/helpers/mongo-helper')
 const env = require('./config/env')
 MongoHelper.connect(env.mongoUrl).then(() => {
   const app = require('./config/app')
-  app.listen(5858, () => {
-    console.log('Server is running on port 5858')
+  app.listen(env.port, () => {
+    console.log(`Server is running at http://localhost:${env.port}`)
   })
 }).catch((error) => {
   console.error('Error connecting to MongoDB:', error)
