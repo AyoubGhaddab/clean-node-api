cdedddd80cd4eaafda6d4f1d4fca13ce3bddceee
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-15 10:39:27 +0200

    disable x-powered-by header

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
+const app = require('./config/app')
 
+app.get('/api/mango', (req, res) => {
+  res.send('mango')
+})
 app.listen(5858, () => {
   console.log('Server is running on port 5858')
 })
